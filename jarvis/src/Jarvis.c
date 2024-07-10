7f26734f96bd033ad9206a48e5502569aaa06fa7
Mriganka Patra <mriganka9432@gmail.com>
2024-07-10 00:38:57 -0400

    add ram usage in jarvis and mon.c

--- jarvis/src/Jarvis.c
+++ jarvis/src/Jarvis.c
@@ -40,6 +40,7 @@
 
 int peak_usage;
 int avg_usage;
+unsigned long mem_total, mem_free, mem_used;
 
 extern void* monitor_cpu_usage(void* arg);
 
@@ -656,6 +657,11 @@ int main(int argc, char **argv){
 
   printf("\nAvg CPU Usage: %d%%\n", avg_usage);
 
+  //////////////////////////////
+
+    ///////////////////////////////
+  mem_used = mem_total - mem_free;
+  printf("Memory Usage: %lu kB used out of %lu kB\n", mem_used, mem_total);
   //////////////////////////////
   
   return 0;
